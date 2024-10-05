ae44a6e0653a12813dd8d67c5de8c8a3c0c41b28
Théo Constant <to.constant@gmail.com>
2024-10-06 01:14:15 +0200

    Add trait icon animation

--- Assets/TraitIcon.cs
+++ Assets/TraitIcon.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class TraitIcon : MonoBehaviour
 {
@@ -8,18 +9,52 @@ public class TraitIcon : MonoBehaviour
     public Image Icon;
     public TMP_Text Text;
     public TraitData Data;
+    public float BumpDuration;
+    public float GreatBumpDuration;
 
     public void Initialize(TraitData data, TraitInfo info)
     {
         Data = data;
         Icon.sprite = data.Icon;
-        SetStatus(info.Status);
+        SetStatus(info.Status, force: true);
     }
 
-    public void SetStatus(ETraitStatus status)
+    public void SetStatus(ETraitStatus status, bool force = false)
     {
-        gameObject.SetActive(status != ETraitStatus.NotPossessed);
-        GreatBackground.SetActive(status == ETraitStatus.Great);
+        if(force)
+        {
+            gameObject.SetActive(status != ETraitStatus.NotPossessed);
+            GreatBackground.transform.localScale = status == ETraitStatus.Great ? Vector3.one : Vector3.one * 0.7f;
+        }
+        else
+        {
+            if(status == ETraitStatus.NotPossessed
+                && gameObject.activeSelf
+                )
+            {
+                DOTween.Sequence()
+                    .Join(transform.DOScale(Vector3.zero, BumpDuration).SetEase(Ease.InBack))
+                    .Join(transform.DORotate(new Vector3(0, 0, 20f), BumpDuration).SetEase(Ease.InBack))
+                    .OnComplete(() => gameObject.SetActive(false));
+            }
+            else if( status != ETraitStatus.NotPossessed
+                && !gameObject.activeSelf
+                )
+            {
+                DOTween.Sequence()
+                    .Join(transform.DOScale(Vector3.one, BumpDuration).SetEase(Ease.OutBack))
+                    .Join(transform.DORotate(new Vector3(0, 0, 0f), BumpDuration).SetEase(Ease.OutBack))
+                    .OnStart(() =>
+                    {
+                        gameObject.SetActive(true);
+                        transform.localScale = Vector3.zero;
+                        transform.rotation = Quaternion.Euler(0, 0, 20f);
+                    });
+            }
+
+            GreatBackground.transform.DOScale(status == ETraitStatus.Great ? Vector3.one : Vector3.one * 0.7f, GreatBumpDuration).SetEase(Ease.OutQuint);
+        }
+
         Text.text = status == ETraitStatus.Unknown ? "?" : Data.Name;
     }
 }
