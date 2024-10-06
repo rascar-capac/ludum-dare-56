9a54b8b028ddb734210a9b46c49bace4ca682181
Théo Constant <to.constant@gmail.com>
2024-10-06 12:33:21 +0200

    Add random movement

--- Assets/Bogbog.cs
+++ Assets/Bogbog.cs
@@ -1,9 +1,13 @@
-using System.Linq;
+using DG.Tweening;
 using UnityEngine;
 
 public class Bogbog : MonoBehaviour
 {
     public SerializableDictionary<TraitData, GameObject> Attributes;
+    public Transform AssignedDestination;
+    public float MetersPerSecond;
+    public float MovementRotationAngle;
+    public float MovementRotationDuration;
 
     public void SetEnabledAttribute(bool is_enabled, TraitData traitType)
     {
@@ -33,8 +37,42 @@ public class Bogbog : MonoBehaviour
         //restore position
     }
 
+    public void AssignDestination(Transform destination)
+    {
+        AssignedDestination = destination;
+        transform.rotation = Quaternion.Euler(0f, 0f, -MovementRotationAngle);
+        transform.DOKill();
+        transform.DORotate(new Vector3(0f, 0f, MovementRotationAngle), MovementRotationDuration).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
+    }
+
+    public void MoveToDestination()
+    {
+        if(AssignedDestination == null)
+        {
+            return;
+        }
+
+        if((AssignedDestination.position - transform.position).sqrMagnitude < 0.1f)
+        {
+            AssignedDestination = null;
+            transform.rotation = Quaternion.identity;
+            transform.DOKill();
+
+            return;
+        }
+
+        Vector3 direction = (AssignedDestination.position - transform.position).normalized;
+        Vector3 translation = MetersPerSecond * Time.deltaTime * direction;
+        transform.Translate(translation);
+    }
+
     private void Awake()
     {
         SetEnabledAllAttributes(false);
+    }
+
+    private void Update()
+    {
+        MoveToDestination();
     }
 }
--- Assets/BogbogsManager.cs
+++ Assets/BogbogsManager.cs
@@ -1,8 +1,13 @@
 using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 public class BogbogsManager : Singleton<BogbogsManager>
 {
     public List<Bogbog> Bogbogs;
+    public SerializableDictionary<Transform, Bogbog> DestinationPoints;
+    public SerializableDictionary<Bogbog, float> NextDestinationAssignmentTimes;
+    public FloatRange MinMaxDurationBeforeNextDestination;
 
     public void SetEnabledAttribute(bool is_enabled, TraitData traitType)
     {
@@ -49,11 +54,48 @@ public class BogbogsManager : Singleton<BogbogsManager>
         }
     }
 
+    public void HandleDestinations()
+    {
+        foreach(Bogbog bogbog in Bogbogs)
+        {
+            if(!NextDestinationAssignmentTimes.TryGetValue(bogbog, out float nextTime) || Time.time > nextTime)
+            {
+                Transform previousDestination = bogbog.AssignedDestination;
+                AssignRandomDestination(bogbog);
+                FreeDestination(previousDestination);
+            }
+        }
+    }
+
+    public void AssignRandomDestination(Bogbog bogbog)
+    {
+        List<Transform> availableDestinations = DestinationPoints.Where(point => point.Value == null).Select(point => point.Key).ToList();
+        Transform newDestination = availableDestinations[Random.Range(0, availableDestinations.Count)];
+        bogbog.AssignDestination(newDestination);
+        DestinationPoints[newDestination] = bogbog;
+        NextDestinationAssignmentTimes[bogbog] = Time.time + MinMaxDurationBeforeNextDestination.GetRandomValue();
+    }
+
+    public void FreeDestination(Transform destination)
+    {
+        if(destination == null )
+        {
+            return;
+        }
+
+        DestinationPoints[destination] = null;
+    }
+
     public override void Awake()
     {
         base.Awake();
 
         TraitsManager.Instance.OnTraitStatusChanged.AddListener(TraitsManager_OnTraitStatusChanged);
+    }
+
+    private void Update()
+    {
+        HandleDestinations();
     }
 
     protected override void OnDestroy()
