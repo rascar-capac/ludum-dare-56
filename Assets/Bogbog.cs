e765496a0966fe5355c0ef4c08134a274fd8d07c
Théo Constant <to.constant@gmail.com>
2024-10-06 13:42:10 +0200

    Add spots

--- Assets/Bogbog.cs
+++ Assets/Bogbog.cs
@@ -5,9 +5,12 @@ public class Bogbog : MonoBehaviour
 {
     public SerializableDictionary<TraitData, GameObject> Attributes;
     public Transform AssignedDestination;
+    public bool IsAssignedToSpot;
     public float MetersPerSecond;
     public float MovementRotationAngle;
     public float MovementRotationDuration;
+
+    public bool DestinationIsReached => (AssignedDestination.position - transform.position).sqrMagnitude < 0.1f;
 
     public void SetEnabledAttribute(bool is_enabled, TraitData traitType)
     {
@@ -37,33 +40,59 @@ public class Bogbog : MonoBehaviour
         //restore position
     }
 
-    public void AssignDestination(Transform destination)
+    public void AssignSpot(Transform spot)
     {
+        transform.position = spot.position;
+        IsAssignedToSpot = true;
+    }
+
+    public void AssignDestination(Transform destination, bool instant = false)
+    {
+        if(instant)
+        {
+            transform.position = destination.position;
+        }
+
+        if(destination != null)
+        {
+            IsAssignedToSpot = false;
+        }
+
         AssignedDestination = destination;
-        transform.rotation = Quaternion.Euler(0f, 0f, -MovementRotationAngle);
+
+        UpdateMovementAnimation();
+    }
+
+    public void UpdateMovementAnimation()
+    {
         transform.DOKill();
-        transform.DORotate(new Vector3(0f, 0f, MovementRotationAngle), MovementRotationDuration).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
+
+        if(AssignedDestination == null || DestinationIsReached)
+        {
+            transform.rotation = Quaternion.identity;
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, -MovementRotationAngle);
+            transform.DORotate(new Vector3(0f, 0f, MovementRotationAngle), MovementRotationDuration).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
+        }
     }
 
     public void MoveToDestination()
     {
-        if(AssignedDestination == null)
+        if(AssignedDestination == null || DestinationIsReached)
         {
-            return;
-        }
-
-        if((AssignedDestination.position - transform.position).sqrMagnitude < 0.1f)
-        {
-            AssignedDestination = null;
-            transform.rotation = Quaternion.identity;
-            transform.DOKill();
-
             return;
         }
 
         Vector3 direction = (AssignedDestination.position - transform.position).normalized;
         Vector3 translation = MetersPerSecond * Time.deltaTime * direction;
         transform.Translate(translation);
+
+        if(DestinationIsReached)
+        {
+            UpdateMovementAnimation();
+        }
     }
 
     private void Awake()
--- Assets/BogbogsManager.cs
+++ Assets/BogbogsManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BogbogsManager : Singleton<BogbogsManager>
 {
     public List<Bogbog> Bogbogs;
+    public List<Transform> Spots;
     public SerializableDictionary<Transform, Bogbog> DestinationPoints;
     public SerializableDictionary<Bogbog, float> NextDestinationAssignmentTimes;
     public FloatRange MinMaxDurationBeforeNextDestination;
@@ -15,6 +16,96 @@ public class BogbogsManager : Singleton<BogbogsManager>
         {
             bogbog.SetEnabledAttribute(is_enabled, traitType);
         }
+    }
+
+    public void SaveCurrentState()
+    {
+        foreach(Bogbog bogbog in Bogbogs)
+        {
+            bogbog.SaveCurrentState();
+        }
+    }
+
+    public void RestorePreviousState()
+    {
+        foreach(Bogbog bogbog in Bogbogs)
+        {
+            bogbog.RestorePreviousState();
+        }
+    }
+
+    public void AssignSpots()
+    {
+        int spotsToAssignCount = Random.Range(0, Mathf.Min(Spots.Count, Bogbogs.Count) + 1);
+
+        List<Bogbog> availableBogbogs = new(Bogbogs);
+        List<Transform> availableSpots = new(Spots);
+
+        for(int assignmentIndex = 0; assignmentIndex < spotsToAssignCount; assignmentIndex++)
+        {
+            Bogbog bogbog = availableBogbogs[Random.Range(0, availableBogbogs.Count)];
+            Transform spot = availableSpots[Random.Range(0, availableSpots.Count)];
+            bogbog.AssignSpot(spot);
+            AssignDestination(bogbog, null);
+            availableBogbogs.Remove(bogbog);
+            availableSpots.Remove(spot);
+        }
+
+        List<Transform> availableDestinationPoints = new(DestinationPoints.Keys);
+
+        foreach(Bogbog bogbog in availableBogbogs)
+        {
+            Transform point = availableDestinationPoints[Random.Range(0,availableDestinationPoints.Count)];
+            AssignDestination(bogbog, point, instant: true);
+            availableDestinationPoints.Remove(point);
+        }
+    }
+
+    public void HandleDestinations()
+    {
+        foreach(Bogbog bogbog in Bogbogs)
+        {
+            if(bogbog.IsAssignedToSpot)
+            {
+                continue;
+            }
+
+            if(!NextDestinationAssignmentTimes.TryGetValue(bogbog, out float nextTime) || Time.time > nextTime)
+            {
+                AssignDestination(bogbog, GetRandomDestination());
+            }
+        }
+    }
+
+    public Transform GetRandomDestination()
+    {
+        List<Transform> availableDestinations = DestinationPoints.Where(point => point.Value == null).Select(point => point.Key).ToList();
+        Transform newDestination = availableDestinations[Random.Range(0, availableDestinations.Count)];
+
+        return newDestination;
+    }
+
+    public void AssignDestination(Bogbog bogbog, Transform destination, bool instant = false)
+    {
+        Transform previousDestination = bogbog.AssignedDestination;
+        bogbog.AssignDestination(destination, instant);
+        FreeDestination(previousDestination);
+        NextDestinationAssignmentTimes[bogbog] = Time.time + MinMaxDurationBeforeNextDestination.GetRandomValue();
+
+        if(destination != null)
+        {
+            DestinationPoints[destination] = bogbog;
+        }
+    }
+
+    public void FreeDestination(Transform destination)
+    {
+        if(destination == null )
+        {
+            return;
+        }
+
+        DestinationPoints[destination] = null;
     }
 
     private void TraitsManager_OnTraitStatusChanged(TraitData traitData, ETraitStatus status)
@@ -38,59 +129,25 @@ public class BogbogsManager : Singleton<BogbogsManager>
         }
     }
 
-    public void SaveCurrentState()
+    private void ParametersManager_OnCommited()
     {
-        foreach(Bogbog bogbog in Bogbogs)
-        {
-            bogbog.SaveCurrentState();
-        }
+        AssignSpots();
     }
 
-    public void RestorePreviousState()
+    private void ParametersManager_OnPreviewed(IReadOnlyDictionary<ParameterData, float> parameters, int tickCount)
     {
-        foreach(Bogbog bogbog in Bogbogs)
-        {
-            bogbog.RestorePreviousState();
-        }
-    }
-
-    public void HandleDestinations()
-    {
-        foreach(Bogbog bogbog in Bogbogs)
-        {
-            if(!NextDestinationAssignmentTimes.TryGetValue(bogbog, out float nextTime) || Time.time > nextTime)
-            {
-                Transform previousDestination = bogbog.AssignedDestination;
-                AssignRandomDestination(bogbog);
-                FreeDestination(previousDestination);
-            }
-        }
-    }
-
-    public void AssignRandomDestination(Bogbog bogbog)
-    {
-        List<Transform> availableDestinations = DestinationPoints.Where(point => point.Value == null).Select(point => point.Key).ToList();
-        Transform newDestination = availableDestinations[Random.Range(0, availableDestinations.Count)];
-        bogbog.AssignDestination(newDestination);
-        DestinationPoints[newDestination] = bogbog;
-        NextDestinationAssignmentTimes[bogbog] = Time.time + MinMaxDurationBeforeNextDestination.GetRandomValue();
-    }
-
-    public void FreeDestination(Transform destination)
-    {
-        if(destination == null )
-        {
-            return;
-        }
-
-        DestinationPoints[destination] = null;
+        AssignSpots();
     }
 
     public override void Awake()
     {
         base.Awake();
 
+        AssignSpots();
+
         TraitsManager.Instance.OnTraitStatusChanged.AddListener(TraitsManager_OnTraitStatusChanged);
+        ParametersManager.Instance.OnCommited.AddListener(ParametersManager_OnCommited);
+        ParametersManager.Instance.OnPreviewed.AddListener(ParametersManager_OnPreviewed);
     }
 
     private void Update()
@@ -105,6 +162,12 @@ public class BogbogsManager : Singleton<BogbogsManager>
         if(TraitsManager.HasInstance)
         {
             TraitsManager.Instance.OnTraitStatusChanged.RemoveListener(TraitsManager_OnTraitStatusChanged);
+        }
+
+        if(ParametersManager.HasInstance)
+        {
+            ParametersManager.Instance.OnCommited.AddListener(ParametersManager_OnCommited);
+            ParametersManager.Instance.OnPreviewed.AddListener(ParametersManager_OnPreviewed);
         }
     }
 }
